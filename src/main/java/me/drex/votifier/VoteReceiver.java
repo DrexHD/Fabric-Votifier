89056bb92bac6be7e9ab5def7cad5517e9fddad9
Drex <nicknamedrex@gmail.com>
2023-09-29 18:39:49 +0200

    Use current mod version

--- src/main/java/me/drex/votifier/VoteReceiver.java
+++ src/main/java/me/drex/votifier/VoteReceiver.java
@@ -72,7 +72,7 @@ public class VoteReceiver extends Thread {
                 InputStream in = socket.getInputStream();
 
                 // Send them our version.
-                writer.write("VOTIFIER 1.1.6");
+                writer.write("VOTIFIER " + Votifier.VERSION);
                 writer.newLine();
                 writer.flush();
 
